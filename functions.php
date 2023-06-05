9d73d4eeab6b25cfb26903fee0e9d0c83a43d3cd
sakhawat9 <sakhawathossain7969@gmail.com>
2023-06-05 18:09:31 +0600

    add gallery

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(get_theme_file_path("/inc/tgm.php"));
+require_once( get_theme_file_path( "/inc/attachments.php" ) );
 
 if(site_url() == "http://demo.lwhh.com") {
     define("VERSION", time());
@@ -37,3 +38,18 @@ function philosophy_assets(){
 };
 
 add_action("wp_enqueue_scripts", "philosophy_assets");
+
+function philosophy_pagination() {
+    global $wp_query;
+    $links = paginate_links( array(
+        'current'  => max( 1, get_query_var( 'paged' ) ),
+        'total'    => $wp_query->max_num_pages,
+        'type'     => 'list',
+        'mid_size' => 3
+    ) );
+    $links = str_replace( "page-numbers", "pgn__num", $links );
+    $links = str_replace( "<ul class='pgn__num'>", "<ul>", $links );
+    $links = str_replace( "next pgn__num", "pgn__next", $links );
+    $links = str_replace( "prev pgn__num", "pgn__prev", $links );
+    echo $links;
+}
--- template-parts/common/navigation.php
+++ template-parts/common/navigation.php
@@ -7,11 +7,15 @@
     <h2 class="header__nav-heading h6"><?php _e("Site Navigation", "philosophy") ?></h2>
 
     <?php
-    wp_nav_menu(array(
+    $philosophy_menu = wp_nav_menu(array(
         'theme_location'    => 'topmenu',
         'menu_id'           => 'topmenu',
-        'menu_class'        => "header__nav"
-    ))
+        'menu_class'        => "header__nav",
+        "echo"              => false,
+    ));
+
+    $philosophy_menu = str_replace("menu-item-has-children", "menu-item-has-children has-children", $philosophy_menu);
+    echo $philosophy_menu;
     ?>
 
     <a href="#0" title="Close Menu" class="header__overlay-close close-mobile-menu"><?php _e("Close", "philosophy") ?></a>

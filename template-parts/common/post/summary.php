67ef2692d550557d7fb1f49db4a0dcf26f9ef968
sakhawat9 <sakhawathossain7969@gmail.com>
2023-06-05 16:02:51 +0600

    update content

---
+++ template-parts/common/post/summary.php
@@ -0,0 +1,21 @@
+<div class="entry__text">
+    <div class="entry__header">
+
+        <div class="entry__date">
+            <a href="single-standard.html">December 15, 2017</a>
+        </div>
+        <h1 class="entry__title"><a href="single-standard.html">Just a Standard Format Post.</a></h1>
+
+    </div>
+    <div class="entry__excerpt">
+        <p>
+            Lorem ipsum Sed eiusmod esse aliqua sed incididunt aliqua incididunt mollit id et sit proident dolor nulla sed commodo est ad minim elit reprehenderit nisi officia aute incididunt velit sint in aliqua...
+        </p>
+    </div>
+    <div class="entry__meta">
+        <span class="entry__meta-links">
+            <a href="category.html">Design</a>
+            <a href="category.html">Photography</a>
+        </span>
+    </div>
+</div>

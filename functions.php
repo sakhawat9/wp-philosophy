066de7da79add3fe4f25cc1ff5ff8244eb651726
sakhawat9 <sakhawathossain7969@gmail.com>
2023-06-06 12:20:53 +0600

    add socail icons

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 require_once(get_theme_file_path("/inc/tgm.php"));
 require_once( get_theme_file_path( "/inc/attachments.php" ) );
+require_once( get_theme_file_path( "/widgets/social-icons-widget.php" ) );
 
 if(site_url() == "http://demo.lwhh.com") {
     define("VERSION", time());
